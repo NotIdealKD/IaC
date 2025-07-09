0c06d53e2c73e1648b3a3c895e276de5047a5322
kd-sat <kamil.dziama@gmail.com>
2025-07-09 15:22:55 +0100

    Module and .gitignore changes

--- Terraform/VM/azvmwindows.tf
+++ Terraform/VM/azvmwindows.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "terraform-providers/azurerm"
+        source = "hashicorp/azurerm"
         version = ">= 3.7.0"
     }
   }
@@ -16,9 +16,11 @@ terraform {
 }
 provider "azurerm" {
   features {}
+  subscription_id = "4d5d3655-8274-4f6a-95c8-6c295432f8fa"
   use_oidc = true
 }
 
 resource "azurerm_resource_group" "prod-vm-rsg01" {
   name = var.resource_group_name
+  location = var.location
 }
