984456fe3c1b839e658694c1edd27b6311e61a41
kd-sat <kamil.dziama@gmail.com>
2025-08-07 13:56:43 +0100

    Variable changes

--- Terraform/modules/resourceGroup/variables.tf
+++ Terraform/modules/resourceGroup/variables.tf
@@ -1,4 +1,6 @@
 #Location 
-variable "location" {}
+variable "location" {
+  default = uk_south
+}
 #Resource group vars
 variable "resource_group_name" {}
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,2 +1,7 @@
 #Global vars
 variable "subscription_id" {}
+
+#Location 
+variable "location" {}
+#Resource group vars
+variable "resource_group_name" {}
