a85f1dec8b445e4ed94583964ec8dd1ae85fb79e
KD-sat <kamil.dziama@gmail.com>
2025-09-29 12:05:05 +0100

    terraform init

--- Terraform/azvm/main.tf
+++ Terraform/azvm/main.tf
@@ -1,12 +1,12 @@
 module "rg" {
-  source = "./modules/resourceGroup"
+  source = "../modules/resourceGroup"
   resource_group_name = var.resource_group_name
   location = var.location
 }
 
 #
 module "vnet" {
-  source = "./modules/vnet"
+  source = "../modules/vnet"
   location = var.location
   resource_group_name = module.rg.resource_group_name
   address_space = var.address_space
