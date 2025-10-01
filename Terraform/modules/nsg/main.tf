b755907463c7715b109bc4f5f11e36bc496636de
kd-sat <kamildziama@gmail.com>
2025-10-01 23:33:01 +0100

    nsg module

--- Terraform/modules/nsg/main.tf
+++ Terraform/modules/nsg/main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_network_security_group" "NSG" {
-  resource_group_name = azurerm_resource_group.prod_vm_rsg01.name
+  resource_group_name = var.resource_group_name
   name                = var.vmsubnet_01_name
   location            = var.location
 }
 
 resource "azurerm_subnet_network_security_group_association" "example" {
-  subnet_id                 = azurerm_subnet.vmsubnet_01.id
-  network_security_group_id = azurerm_network_security_group.vmsubnet_01_NSG.id
+  subnet_id                 = var.subnet_id
+  network_security_group_id = var.nsg_id
 }
