981f5058a1b75c14cd5db6b3827cea47b6e89d0e
KD-sat <kamil.dziama@gmail.com>
2025-11-13 15:37:29 +0000

    subnet module vars

---
+++ Terraform/modules/subnet/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {}
+variable "subnet" {
+  description = "Address space for the subnet in CIDR notation"
+}
+variable "vnet_name" {}
+variable "subnet_name" {
+  description = "Name of the subnet"
+}
