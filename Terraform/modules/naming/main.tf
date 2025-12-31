9b2aa3af6f467adad1ee090cc0d26cc08313cfd1
kd-sat <kamildziama@gmail.com>
2025-12-31 00:18:06 +0000

    Small tweaks to main.tf for naming module

--- Terraform/modules/naming/main.tf
+++ Terraform/modules/naming/main.tf
@@ -2,7 +2,7 @@ locals {
   elements = compact([
     var.prefix,
     var.resource_type,
-    resource_role != null ? var.resource_role : null,
+    var.resource_role != null ? var.resource_role : null,
     substr(md5("${var.prefix}-${var.resource_role}"), 0, 4)
   ])
   name = lower(join("-", local.elements))
