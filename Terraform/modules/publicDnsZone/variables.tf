de52b0ef1dd005d2ee909fc196855db1ea5bfba9
KD-sat <kamil.dziama@gmail.com>
2026-01-07 14:53:52 +0000

    Initial files

---
+++ Terraform/modules/publicDnsZone/variables.tf
@@ -0,0 +1,15 @@
+#Resource group vars
+variable "resource_group_name" {
+  description = "Name of the resource group"
+}
+
+variable "dns_zone_name" {
+  type = string
+  description = "DNS Zone name"
+}
+
+variable "tags" {
+  description = "Tags to apply to resource"
+  type        = map(string)
+  default     = {}
+}
