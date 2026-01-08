f4b492086dca574ac2da2fca7e7816c11d921692
kd-sat <kamildziama@gmail.com>
2026-01-08 22:12:35 +0000

    DNS records module

---
+++ Terraform/modules/publicDnsZoneRecords/main.tf
@@ -0,0 +1,7 @@
+resource "azapi_resource" "dns_record" {
+  for_each = var.records
+  type = "microsoft.networks/dnsZones/${each.value.type}@2018-05-01"
+  parent_id = var.parent_id
+  name = var.name
+  body = var.body
+}
