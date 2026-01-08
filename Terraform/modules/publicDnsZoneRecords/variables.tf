3534a06761196a36b8a52c0fc75e2bfc79afef21
kd-sat <kamildziama@gmail.com>
2026-01-08 22:12:45 +0000

    DNS record vars

---
+++ Terraform/modules/publicDnsZoneRecords/variables.tf
@@ -0,0 +1,10 @@
+variable "records" {
+  description = "Map of DNS records to create. Key is the record name, value is an object with record details."
+  type = map(object({
+    zone_name           = string
+    resource_group_name = string
+    type                = string
+    ttl                 = optional(number)
+    values              = list(string)
+  }))
+}
